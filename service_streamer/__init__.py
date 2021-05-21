a24ef73604b11baa344d69272eb76c40903ec6dd
Zebartin <1500012724@pku.edu.cn>
2021-05-21 15:17:09 +0800

    feat: add in __init__.py

--- service_streamer/__init__.py
+++ service_streamer/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf-8
 # Created by Meteorix at 2019/7/22
 
-from .service_streamer import ThreadedStreamer, Streamer, RedisStreamer, RedisWorker, run_redis_workers_forever
+from .service_streamer import ThreadedStreamer, Streamer, RedisStreamer, RedisWorker, run_redis_workers_forever, run_redis_workers_without_model_forever
 from .managed_model import ManagedModel
